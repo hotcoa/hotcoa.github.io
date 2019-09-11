9444d8c6d4eef40f9da0f1dd889c168a973cf84c
Hiyo Kate <shin@hiyos-imac.lan>
2019-09-10 23:13:22 -0700

    a

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-   $('.map').maphilight({ stroke: true, fill:true, fillColor: 'a986e6', fillOpacity: 0.5, strokeWidth: 5, strokeColor: 'a986e6', strokeOpacity: 0.5});
+   //$('.map').maphilight({ stroke: true, fill:true, fillColor: 'a986e6', fillOpacity: 0.5, strokeWidth: 5, strokeColor: 'a986e6', strokeOpacity: 0.5});
 });
 
 /*
@@ -35,17 +35,20 @@ $(function() {
         var left=coorA[0];
         var top=coorA[1];
 
+		var spaceIdx = 0;
 		var link;
 		var $span;
         switch (txt) {
             case 'Who am i':
                 link = 'html/aboutme.html';
 				break;
-			case 'Art+tech':
-                link = 'html/arttech.html';
+			case 'Art + tech':
+				link = 'html/arttech.html';
+				spaceIdx = 1;
 				break;
 			case 'UX/UI':
-                link = 'html/design.html';
+				link = 'html/design.html';
+				spaceIdx = 2;
                 break;
             default:
                 break;
@@ -53,10 +56,15 @@ $(function() {
 		if (link) {
 			$span=$(`<a class="mymap_title" href="${link}">`+txt+'</a>');
 		} else {
-			$span=$(`<span class="mymap_title">`+txt+'</span>');
+			$span=$(`<span class="mymap_title_nolink">`+txt+'</span>');
 		}
-        
-        $span.css({top: Number(top)+20+'px', left: Number(left)+13+'px', position:'absolute'});
+		
+		var spacing = [
+			{ top: 30, left: 35 },
+			{ top: 20, left: 13 },
+			{ top: 36, left: 20 },
+		]
+        $span.css({top: Number(top) + spacing[spaceIdx].top + 'px', left: Number(left) + spacing[spaceIdx].left + 'px', position:'absolute'});
         $span.appendTo('#mymap');
     })  
 })
