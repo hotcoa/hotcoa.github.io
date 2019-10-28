94eaff8e208261da9b8e0e92519c84a0b4e97c3d
Hiyo Kate <shin@hiyos-imac.lan>
2019-10-28 02:03:55 -0700

    i

--- infiniteScroll2/script.js
+++ infiniteScroll2/script.js
@@ -265,7 +265,52 @@ const initIntersectionObserver = () => {
   observer.observe(document.querySelector(`#tile-${listSize - 1}`));
 }
 
+
+const testTouch = () => { 
+    var touchsurface = document.getElementById('touchsurface'),
+        startX,
+        startY,
+        dist,
+        threshold = 150, //required min distance traveled to be considered swipe
+        allowedTime = 200, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime
+ 
+    function handleswipe(isrightswipe){
+        if (isrightswipe)
+            touchsurface.innerHTML = 'Congrats, you\'ve made a <span style="color:red">right swipe!</span>'
+        else{
+            touchsurface.innerHTML = 'Condition for right swipe not met yet'
+        }
+    }
+ 
+    touchsurface.addEventListener('touchstart', function(e){
+        touchsurface.innerHTML = '';
+        var touchobj = e.changedTouches[0];
+        dist = 0;
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
+        e.preventDefault();
+    }, false)
+ 
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault(); // prevent scrolling when inside DIV
+    }, false)
+ 
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0];
+        dist = touchobj.pageX - startX; // get total dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
+        // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+        var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100);
+        handleswipe(swiperightBol);
+        e.preventDefault();
+    }, false)
+}
+
 window.onload = function() {
+    testTouch();
     var currDate = new Date();
     var hourMinFormat = currDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
     console.log(hourMinFormat);
